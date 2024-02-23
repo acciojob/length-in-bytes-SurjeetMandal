fa5aca29e6756755c1685d1d923acb719c1c52e7
SurjeetMandal <surjeetmandal698@gmail.com>
2024-02-23 20:32:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const byteSize = (str) => {
   // write your code here
+	let count = 0;
+	for (let i = 0; i < str.length; i++) {
+		count ++;
+	}
+	return count;
 };
-
-// Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
+// Do not change the code below
+
